b2695e040562de97c5438e9dcb43f828a3c47889
Klaudia Kieliszczyk <128413369+klaudiakieliszczykk@users.noreply.github.com>
2025-07-11 13:36:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    if (target) {
-      target.scrollIntoView({ behavior: "smooth" });
-    }
-  });
-});
+
+console.log("Strona portfolio fotografii załadowana.");
